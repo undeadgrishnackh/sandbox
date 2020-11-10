e0939701fc9b2a0d18bf25fa94245598f43cbc62
Michele Brissoni <michele.brissoni@ibm.com@Micheles-MacBook-Pro-2.local>
2020-11-10 14:28:18 +0100

    Sandbox test seed stage

---
+++ src/sandbox.js
@@ -0,0 +1,7 @@
+const sandbox = () => {
+
+};
+
+module.exports = {
+  sandbox
+}
---
+++ test/sandbox.test.js
@@ -0,0 +1,7 @@
+const sandbox = require('../src/sandbox');
+
+describe('We wanna test first...', () => {
+  it('so we start to expect something', () => {
+    expect(sandbox()).toBe(true);
+  });
+});
